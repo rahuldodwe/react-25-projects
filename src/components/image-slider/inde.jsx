612e6d4415ddab6b3ececd085fa9148c0b340f9c
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-08-28 13:12:37 +0530

    04-image-slider started

---
+++ src/components/image-slider/inde.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+
+export default function ImageSlider({url, limit}) {
+    const [images, setImages] = useState([])
+
+    return(
+        <>
+        <div className="container"></div>
+        </>
+    )
+
+}
