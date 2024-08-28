8c1a50d56803e8902c964f62885a0b60ee84f138
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-08-28 15:55:38 +0530

    update02

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-
-import './App.css'
-import Accordian from './components/accordian'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
-
   return (
     <>
+      {/* Accordian Component */}
+      {/* <Accordian/> */}
 
-    {/* Accordian Component */}
-    {/* <Accordian/> */}
-    
-    {/* Random color component */}
-    {/* <RandomColor/> */}
-    {/* Star-Rating Component */}
-    <StarRating/>
+      {/* Random color component */}
+      {/* <RandomColor/> */}
+
+      {/* Star-Rating Component */}
+      {/* <StarRating/> */}
+
+      {/* Image Slider Component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
